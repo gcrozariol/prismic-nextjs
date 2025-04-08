401aeeef6b7ca465af06d167b8aada83bd06c905
Guilherme Henrique Crozariol <guicrozariol@gmail.com>
2025-04-08 17:29:07 -0300

    initial commit

---
+++ src/slices/Button/index.tsx
@@ -0,0 +1,30 @@
+import { FC } from 'react'
+import { Content } from '@prismicio/client'
+import { SliceComponentProps } from '@prismicio/react'
+import { Button as ShadcnButton } from '@/components/ui/button'
+import Link from 'next/link'
+
+/**
+ * Props for `Button`.
+ */
+export type ButtonProps = SliceComponentProps<Content.ButtonSlice>
+
+/**
+ * Component for "Button" Slices.
+ */
+const Button: FC<ButtonProps> = ({ slice }) => {
+  return (
+    <ShadcnButton
+      className="bg-amber-500 p-10"
+      asChild
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <Link href={slice.primary.link.text || '/another-page'}>
+        {slice.primary.label}
+      </Link>
+    </ShadcnButton>
+  )
+}
+
+export default Button
